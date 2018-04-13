c7f8c2dd33d4c5aa9b03550496d590b9fa8b1ca6
Ferré Nicolás <36193611+ferreenicolas@users.noreply.github.com>
2018-04-13 11:09:04 -0300

    Add files via upload

---
+++ ejerciciosguia/main.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    /*  EJERCICIO 3
+    int numeroUno = 5;
+    int numeroDos = 2;
+    int producto;
+    int cuadrado;
+
+
+    printf("La variable numero uno es: %d\n", numeroUno);
+    printf("La variable numero dos es: %d\n", numeroDos);
+
+    system("pause");
+    system("cls");
+
+    producto = numeroUno * numeroDos;
+
+    printf("El producto de ambos numeros es: %d\n", producto);
+
+    system("pause");
+    system("cls");
+
+    cuadrado = numeroUno * numeroUno;
+
+    printf("El cuadrado del numero uno es: %d\n", cuadrado);
+
+    */
+    float numeroUno;
+    float numeroDos;
+    float resultado;
+
+    printf("Ingrese numero uno: \n");
+    scanf("%f", %numeroUno);
+    printf("Ingrese numero dos: \n");
+    scanf("%f", %numeroDos);
+
+    resultado = numeroUno - numeroDos;
+
+    if(resultado > 0)
+        {
+        printf("Resultado positivo.");
+        }
+        else
+            {
+            if(resultado < 0)
+                {
+                printf("Resultado negativo.");
+                }
+            }
+
+
+    return 0;
+}
